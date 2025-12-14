5590345ab66613fdf19cd2a80ff51c1055b816b2
Nuno Caldeira <nunocapelocaldeira@gmail.com>
2025-12-14 19:12:30 +0000

    upload

---
+++ data.js
@@ -0,0 +1,480 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-mercado-dos-lavradores",
+      "name": "Mercado dos Lavradores",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.07604513970190396,
+        "pitch": 0.5273287497982295,
+        "fov": 1.3419674740723089
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.48248852568517187,
+          "pitch": 0.08682648030232798,
+          "rotation": 3.141592653589793,
+          "target": "2-rua-ferno-ornelas"
+        },
+        {
+          "yaw": -0.8231972747012151,
+          "pitch": 0.4352667825999088,
+          "rotation": 3.141592653589793,
+          "target": "3-rua-dom-carlos-i"
+        },
+        {
+          "yaw": -1.0180585363953334,
+          "pitch": 0.21431303532898127,
+          "rotation": 3.141592653589793,
+          "target": "4-frente--empresa-de-eletricidade"
+        },
+        {
+          "yaw": -2.780535705813003,
+          "pitch": 0.4849781036635967,
+          "rotation": 3.141592653589793,
+          "target": "1-rua-latino-coelho"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0120877724737731,
+          "pitch": 0.4600579520199801,
+          "title": "Rua Dom Carlos I<br>",
+          "text": "Brinquedos, bebidas e Comidas"
+        },
+        {
+          "yaw": -1.1252280104967038,
+          "pitch": 0.2366477790484236,
+          "title": "Frente à empresa<br>de eletricidade",
+          "text": "Ramagens e pinheiros"
+        },
+        {
+          "yaw": 0.46666289189670884,
+          "pitch": 0.1930234754340585,
+          "title": "Rua Fernão Ornelas<br>",
+          "text": "Bebidas e Comidas"
+        },
+        {
+          "yaw": 0.03403144216802545,
+          "pitch": 1.5482603336470744,
+          "title": "Mercado dos Lavradores<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -2.20551243161502,
+          "pitch": 0.9367275889264537,
+          "title": "Rua Latino Coelho<br>",
+          "text": "Produtos hortofrutícolas"
+        }
+      ]
+    },
+    {
+      "id": "1-rua-latino-coelho",
+      "name": "Rua Latino Coelho",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -0.0778694379904099,
+        "pitch": 0.21208653664820787,
+        "fov": 1.3419674740723089
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22390080857134542,
+          "pitch": 0.44464158453629565,
+          "rotation": 3.141592653589793,
+          "target": "0-mercado-dos-lavradores"
+        },
+        {
+          "yaw": 0.3025477783043531,
+          "pitch": 0.10493941532183904,
+          "rotation": 3.141592653589793,
+          "target": "2-rua-ferno-ornelas"
+        },
+        {
+          "yaw": -0.714083649304829,
+          "pitch": 0.20301110463257288,
+          "rotation": 3.141592653589793,
+          "target": "4-frente--empresa-de-eletricidade"
+        },
+        {
+          "yaw": -0.46246423186846464,
+          "pitch": 0.30348904400120347,
+          "rotation": 3.141592653589793,
+          "target": "3-rua-dom-carlos-i"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.25992631195773264,
+          "pitch": 0.21482517200645646,
+          "title": "Rua Fernão Ornelas<br>",
+          "text": "Bebidas e Comidas"
+        },
+        {
+          "yaw": -0.8149613956084103,
+          "pitch": 0.25696943365181113,
+          "title": "Frente à empresa<br>de eletricidade",
+          "text": "Ramagens e pinheiros"
+        },
+        {
+          "yaw": -0.5547949626524655,
+          "pitch": 0.3792857403304879,
+          "title": "Rua Dom Carlos I<br>",
+          "text": "Brinquedos bebidas e Comidas"
+        },
+        {
+          "yaw": -0.08509116168072062,
+          "pitch": 0.46834285145748566,
+          "title": "Rua Latino Coelho<br>",
+          "text": "Produtos hortofrutícolas"
+        }
+      ]
+    },
+    {
+      "id": "2-rua-ferno-ornelas",
+      "name": "Rua Fernão Ornelas",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": -2.026031560447496,
+        "pitch": 0.495650479888031,
+        "fov": 0.9494798451518032
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6861233761366794,
+          "pitch": 0.4253380856390869,
+          "rotation": 3.141592653589793,
+          "target": "3-rua-dom-carlos-i"
+        },
+        {
+          "yaw": -1.48628819643492,
+          "pitch": 0.3622140617253393,
+          "rotation": 15.707963267948973,
+          "target": "4-frente--empresa-de-eletricidade"
+        },
+        {
+          "yaw": -2.1607357419546744,
+          "pitch": 0.3724763402481006,
+          "rotation": 3.141592653589793,
+          "target": "0-mercado-dos-lavradores"
+        },
+        {
+          "yaw": -2.1714942085574425,
+          "pitch": 0.30142541797102673,
+          "rotation": 3.141592653589793,
+          "target": "1-rua-latino-coelho"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.102316338917305,
+          "pitch": 0.3496982211185333,
+          "title": "Rua Latino Coelho<br>",
+          "text": "Produtos hortofrutícolas"
+        },
+        {
+          "yaw": -2.1159428924625026,
+          "pitch": 0.43512501472135234,
+          "title": "Mercado dos Lavradores<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": -1.7703112105586989,
+          "pitch": 0.38796391421032084,
+          "title": "Rua Dom Carlos I<br>",
+          "text": "Brinquedos, bebidas e Comidas"
+        },
+        {
+          "yaw": -1.5604174698227222,
+          "pitch": 0.32906922949714,
+          "title": "Frente à empresa<br>de eletricidade",
+          "text": "Ramagens e pinheiros"
+        },
+        {
+          "yaw": -2.0988475008337346,
+          "pitch": 0.9034946045340426,
+          "title": "Rua Fernão Ornelas<br>",
+          "text": "Bebidas e Comidas"
+        }
+      ]
+    },
+    {
+      "id": "3-rua-dom-carlos-i",
+      "name": "Rua Dom Carlos I",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 2.7242881409161663,
+        "pitch": 0.5627530183429315,
+        "fov": 1.3419674740723089
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4567687483039222,
+          "pitch": 0.829274451928196,
+          "rotation": 3.141592653589793,
+          "target": "4-frente--empresa-de-eletricidade"
+        },
+        {
+          "yaw": 2.5071873432205027,
+          "pitch": 0.55968615774405,
+          "rotation": 3.141592653589793,
+          "target": "0-mercado-dos-lavradores"
+        },
+        {
+          "yaw": 2.8944256108534177,
+          "pitch": 0.43651848792501724,
+          "rotation": 3.141592653589793,
+          "target": "1-rua-latino-coelho"
+        },
+        {
+          "yaw": 1.0319984921485315,
+          "pitch": 0.3202092893359012,
+          "rotation": 3.141592653589793,
+          "target": "2-rua-ferno-ornelas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.8451915110403956,
+          "pitch": 0.9361635017702401,
+          "title": "Rua Dom Carlos I<br>",
+          "text": "Brinquedos, bebidas e Comidas"
+        },
+        {
+          "yaw": 2.3948342969511316,
+          "pitch": 0.6312721655483333,
+          "title": "Mercado dos Lavradores<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 2.713618543758935,
+          "pitch": 0.7307594811222238,
+          "title": "Rua Latino Coelho<br>",
+          "text": "Produtos hortofrutícolas"
+        },
+        {
+          "yaw": 1.3430726345110102,
+          "pitch": 0.5229957617935987,
+          "title": "Rua Fernão Ornelas<br>",
+          "text": "Bebidas e Comidas"
+        },
+        {
+          "yaw": -1.786740378106547,
+          "pitch": 0.8364263398813847,
+          "title": "Frente à empresa<br>de eletricidade",
+          "text": "Ramagens e pinheiros"
+        }
+      ]
+    },
+    {
+      "id": "4-frente--empresa-de-eletricidade",
+      "name": "Frente à empresa de eletricidade",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 2.469965251116827,
+        "pitch": 0.6668901393030247,
+        "fov": 1.3419674740723089
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1938351080530323,
+          "pitch": 0.8018166522835237,
+          "rotation": 3.141592653589793,
+          "target": "3-rua-dom-carlos-i"
+        },
+        {
+          "yaw": 2.950004301691136,
+          "pitch": 0.329069740013999,
+          "rotation": 3.141592653589793,
+          "target": "1-rua-latino-coelho"
+        },
+        {
+          "yaw": 2.5116722577324007,
+          "pitch": 0.31738697903990243,
+          "rotation": 3.141592653589793,
+          "target": "0-mercado-dos-lavradores"
+        },
+        {
+          "yaw": 1.680707289147608,
+          "pitch": 0.28024070426053527,
+          "rotation": 3.141592653589793,
+          "target": "2-rua-ferno-ornelas"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.041481613061734,
+          "pitch": 1.088541240601188,
+          "title": "Frente à empresa<br>de eletricidade",
+          "text": "Ramagens e pinheiros"
+        },
+        {
+          "yaw": 2.571610260288214,
+          "pitch": 0.7726857990002163,
+          "title": "Rua Dom Carlos I<br>",
+          "text": "Brinquedos, bebidas e Comidas"
+        },
+        {
+          "yaw": 2.736135894051346,
+          "pitch": 0.43867337950810104,
+          "title": "Rua Latino Coelho<br>",
+          "text": "Produtos hortofrutícolas"
+        },
+        {
+          "yaw": 2.5214615180167748,
+          "pitch": 0.4101384537537047,
+          "title": "Mercado dos Lavradores<br>",
+          "text": "<br>"
+        },
+        {
+          "yaw": 1.8454174935627883,
+          "pitch": 0.33417590737267133,
+          "title": "Rua Fernão Ornelas<br>",
+          "text": "Bebidas e Comidas"
+        }
+      ]
+    }
+  ],
+  "name": "Noite do Mercado",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
